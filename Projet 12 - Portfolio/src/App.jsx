ff93692de798447165bb9cd3c735d0645fcb957a
romainBeauger <romain.beauger@gmail.com>
2025-11-02 15:02:31 +0100

    Création du projet

---
+++ Projet 12 - Portfolio/src/App.jsx
@@ -0,0 +1,26 @@
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Skills from './components/Skills'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+
+
+
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Skills />
+      {/* <Projects /> */}
+      {/* <Contact /> */}
+      {/* <Footer />      */}
+    </>
+  )
+}
+
+export default App
